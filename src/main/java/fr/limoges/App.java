721b07bfbafc5a8da33fb94890bc3ee1ce448c39
Frank Dester <frankdester83@gmail.com>
2021-04-03 18:01:53 +0200

    Initial commit

---
+++ src/main/java/fr/limoges/App.java
@@ -0,0 +1,13 @@
+package fr.limoges;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
